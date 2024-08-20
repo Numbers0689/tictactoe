8a060aacce873778bd2831bbad8f6e0f45980be2
Numbers0689 <firewatchsai@gmail.com>
2024-08-20 19:23:26 +0530

    Create gameboard func

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const Game = (function() {
+const gameBoard = (function() {
     //private
-
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let winCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
     //return public var and funcs
+});
+
+const Game = (() => {
+    gameBoard();
 })();
