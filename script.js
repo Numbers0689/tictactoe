81684166a8427b8d034e6b5052bd34700f05c89b
Numbers0689 <firewatchsai@gmail.com>
2024-09-23 23:36:10 +0530

    Create player and gameboard methods

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const prompt = require('prompt-sync')();
+
 const gameBoard = (function() {
     //private
     let board = ['', '', '', '', '', '', '', '', ''];
@@ -12,9 +14,31 @@ const gameBoard = (function() {
         [2, 4, 6]
     ];
 
+    const checkWin = (symbol) => {
+        for (let c in winCombinations) {
+            if (board[c[0]] === symbol && symbol === board[c[1]] && symbol === board[c[2]]) return true;
+        }
+        return false;
+    };
+
+    const writeMove = (symbol, i) => board[i] = symbol;
     //return public var and funcs
+    return {checkWin, writeMove};
+})();
+
+const player = (function(symbol, name) {
+    let turn = false;
+    return {symbol, turn, name};
 });
 
 const Game = (() => {
-    gameBoard();
+    let p1 = player('x', 'sai');
+    let p2 = player('o', 'computer');
+    let symbol, turn = p1;
+    for (let i = 0; i < 9; i++) {
+        index = prompt(`enter index - ${turn.symbol}: `);
+        gameBoard.writeMove(turn.symbol, index);
+        turn = (turn == p1) ? p2 : p1;
+        if (gameBoard.checkWin(turn.symbol)) console.log()
+    }
 })();
